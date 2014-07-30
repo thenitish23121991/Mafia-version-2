104f0669b84f2ef93f47607474e6683a75200749
Nitish Hooda <thenitish23121991@yahoo.in>
2014-07-30 20:49:29 +0530

    done

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1041,7 +1041,7 @@ console.log(data12);
 
 $('.left_game_chat_messages_container').append('<div class="left_game_chat_message_item"><span class="left_game_chat_player_name">'+data12.player+' says:</span><span class="left_game_chat_message"> '+data12.message+'</span></div>');
 $('.left_game_chat_textarea_input').val('');
-var scroll_height = $('.left_game_chat_messages_container')[0].scrollHeight();
+var scroll_height = $('.left_game_chat_messages_container')[0].scrollHeight;
 console.log(scroll_height);
 $('.left_game_chat_messages_container').scrollTop(scroll_height);
 
@@ -1100,7 +1100,7 @@ add_game_message.done(function(data12){
 
 console.log(data12);
 $('.left_game_chat_messages_container').append('<div class="left_game_chat_message_item"><span class="left_game_chat_player_name">'+data12.player+' says:</span><span class="left_game_chat_message"> '+data12.message+'</span></div>');
-var scroll_height = $('.left_game_chat_messages_container')[0].scrollHeight();
+var scroll_height = $('.left_game_chat_messages_container')[0].scrollHeight;
 console.log(scroll_height);
 $('.left_game_chat_messages_container').scrollTop(scroll_height);
 $('.left_game_chat_textarea_input').val('');
