ce2705f22e56e3d55eb2609cce2cdb2af158a51b
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-08 15:31:58 +0530

    done

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -12,6 +12,7 @@ $(this).parent('.middle_games_show_person').css({
 });
 
 $('.middle_home_start_button').bind('click',function(e){
+e.preventDefault();
 var nick = $('.middle_nick_input').val();
 var add_nick = $.ajax({
 url:"/add_nick",
